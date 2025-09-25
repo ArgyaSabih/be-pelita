154df32675ada88a781a3354b23733d473efed97
Christian Kevin Andhika Danidaiva <ckevinadv@gmail.com>
2025-09-25 22:41:24 +0700

    feat: menambahkan login dan autentikasi pengguna

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,1 +1,35 @@
-// Route User
+const express = require("express");
+const {
+  register,
+  login,
+  getProfile,
+  updateProfile
+} = require("../controllers/UserController");
+const { authenticate, authorize } = require("../middlewares/Auth");
+
+const router = express.Router();
+
+// @route   POST /api/users/register
+// @desc    Register a new user
+// @access  Public
+router.post("/register", register);
+
+// @route   POST /api/users/login
+// @desc    Login user
+// @access  Public
+router.post("/login", login);
+
+// @route   GET /api/users/profile
+// @desc    Get user profile
+// @access  Private
+router.get("/profile", authenticate, getProfile);
+
+/*
+* @route   PUT /api/users/profile
+* @desc    Update user profile
+* @access  Private
+*/
+
+router.put("/profile", authenticate, updateProfile);
+
+module.exports = router;
--- utils/JWT.js
+++ utils/JWT.js
@@ -1,4 +1,23 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-exports.generateAuthToken = () => {};
+/**
+ * @param {string} userId
+ * @returns {string}
+ */
+
+exports.generateAuthToken = (userId) => {
+  const payload = {
+    user: {
+      id: userId,
+    },
+  };
+
+  const token = jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+
+  return token;
+};
